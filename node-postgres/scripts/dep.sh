104ff6b7a16bfa043b73bea9ffc1759337789ff3
Emil Culic <emilculic@Emils-MacBook-Pro.local>
2016-02-23 13:27:43 +0000

    Updates provisioning

--- node-postgres/scripts/dep.sh
+++ node-postgres/scripts/dep.sh
@@ -15,4 +15,4 @@ apt-get -yg install git-all
 curl -L http://git.io/n-install | bash
 
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update && sudo apt-get install postgresql-9.4
+sudo -yg apt-get update && apt-get -yg install postgresql-9.4
