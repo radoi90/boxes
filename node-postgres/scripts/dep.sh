8e6f2d4d6ccc9cf7c93a3dae9360f5556c80f71a
Emil Culic <eculic17@gmail.com>
2016-02-23 11:38:20 +0000

    Adds provisioning

--- node-postgres/scripts/dep.sh
+++ node-postgres/scripts/dep.sh
@@ -7,3 +7,9 @@ apt-get -y update
 apt-get -y install curl
 
 # You can install anything you need here.
+
+apt-get install git-all
+
+curl -L http://git.io/n-install | bash
+
+apt-get install postgresql-9.4
